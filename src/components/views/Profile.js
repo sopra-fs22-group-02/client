8f5bf0cde570e621b6016559880d99b0ae85d97d
Pasafa <91162108+Pasafa@users.noreply.github.com>
2022-04-10 01:35:30 +0200

    Sttyle profile page

--- src/components/views/Profile.js
+++ src/components/views/Profile.js
@@ -1,22 +1,45 @@
-import {useEffect, useState} from 'react';
-import {api, handleError} from 'helpers/api';
-import {Spinner} from 'components/ui/Spinner';
-import {Button} from 'components/ui/Button';
+
 import {useHistory} from 'react-router-dom';
 import BaseContainer from "components/ui/BaseContainer";
-import PropTypes from "prop-types";
 import "styles/views/Profile.scss";
 import "styles/views/Game.scss";
+import {Button} from 'components/ui/Button';
 
 
 
 const Profile = () => {
     // use react-router-dom's hook to access the history
-    const history = useHistory();
 
 
     return (
-        <BaseContainer className="game container">
+        <BaseContainer>
+            <div className= "profile card" >
+                <div className= "profile card-header" >
+                    <img className = "profile image" src="/profile.jpeg" />
+                </div>
+                <div className= "profile card-body" >
+                    <p className = "profile first-name" > Paul </p>
+                    <p className = "profile last-name" >  Safari </p>
+                    <p className = "profile email" >  paul.safar@bluewin.ch </p>
+                    <div className= "profile textbox">
+                        <p className = "profile text" > I am a freelance graphic designer with over five years of industry experience.” or “My specialities include logo creation, branding concepts, vector image editing/cropping & Photoshop work.” because it can make you sound generic and boring to potential clients. </p>
+                    </div>
+                <div className= "profile card-footer" >
+                </div>
+                    <Button
+                        width="100%"
+                    >
+                        Edit
+                    </Button>
+
+                </div>
+
+
+
+
+            </div>
+
+
 
         </BaseContainer>
     );
